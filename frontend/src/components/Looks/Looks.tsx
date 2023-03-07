c1873c36b699e2f36759655225643ebb23d04114
Felipe <falmeidagomes13@gmail.com>
2023-03-06 23:40:11 -0300

    Feat: favorite complete

--- frontend/src/components/Looks/Looks.tsx
+++ frontend/src/components/Looks/Looks.tsx
@@ -1,5 +1,5 @@
 import React, {useState} from 'react';
-import Categories from '../Categories/Categories';
+import Categories, {type Clothes} from '../Categories/Categories';
 
 import './Looks.css';
 
@@ -17,7 +17,7 @@ export type CategoriesData = {
 	active: boolean;
 };
 
-function Looks() {
+function Looks(): JSX.Element {
 	const [categories, setCategories] = useState<CategoriesData[]>([
 		{category: 'TODOS', active: true},
 		{category: 'CALÇAS', active: false},
@@ -29,23 +29,60 @@ function Looks() {
 		{category: 'FAVORITOS', active: false},
 	]);
 
-	function handleClickList(index: number) {
-		const copyCategories = categories.map(element => ({...element, active: false}));
-		copyCategories[index].active = true;
-		setCategories(copyCategories);
+	const [clothes, setClothes] = useState<Clothes[]>([
+		{id: 1, category: 'CALÇAS', favorite: false, href: 'https://w7.pngwing.com/pngs/63/280/png-transparent-jeans-denim-slim-fit-pants-bell-bottoms-jeans-blue-fashion-boy.png'},
+		{id: 2, category: 'CALÇAS', favorite: false, href: 'https://w7.pngwing.com/pngs/96/103/png-transparent-t-shirt-pants-clothing-pajamas-loose-pants-adidas-black-top.png'},
+		{id: 3, category: 'SHORTS', favorite: false, href: 'https://w7.pngwing.com/pngs/300/400/png-transparent-jeans-denim-bermuda-shorts-jeans-active-shorts-para-shorts.png'},
+		{id: 4, category: 'SHORTS', favorite: false, href: 'https://w7.pngwing.com/pngs/885/843/png-transparent-bermuda-shorts-button-pants-clothing-boardshorts-button-hybrid-color-trunks.png'},
+		{id: 5, category: 'CAMISETAS', favorite: false, href: 'https://w7.pngwing.com/pngs/29/367/png-transparent-white-crew-neck-t-shirt-t-shirt-leia-organa-hoodie-clothing-t-shirts-tshirt-white-bracelet.png'},
+		{id: 6, category: 'CAMISETAS', favorite: false, href: 'https://w7.pngwing.com/pngs/389/698/png-transparent-t-shirt-top-sleeve-clothing-black-t-shirt-white-crew-neck-t-shirt-tshirt-angle-white.png'},
+		{id: 7, category: 'CALÇADOS', favorite: false, href: 'https://w7.pngwing.com/pngs/1011/776/png-transparent-footwear-shoe-sneakers-sportswear-men-shoes-white-men-shoes-walking-shoe.png'},
+		{id: 8, category: 'CALÇADOS', favorite: false, href: 'https://w7.pngwing.com/pngs/484/755/png-transparent-sneakers-shoe-air-jordan-nike-footwear-jordan-white-orange-basketballschuh.png'},
+		{id: 9, category: 'BLUSAS', favorite: false, href: 'https://w7.pngwing.com/pngs/528/973/png-transparent-white-pullover-hoodie-illustration-mexico-hoodie-bluza-clothing-mercadolibre-hoodie-white-sweatshirt-sweater.png'},
+		{id: 10, category: 'BLUSAS', favorite: false, href: 'https://w7.pngwing.com/pngs/175/902/png-transparent-black-hoodie-black-sweater-hoodie.png'},
+	]);
+
+	function handleClickList(index: number): void {
+		const newCategories = categories.map((category, i) => ({...category, active: i === index}));
+		setCategories(newCategories);
+	}
+
+	function handleClickFavorite(id: number): void {
+		const newClothes = clothes.map(clothe => {
+			if (clothe.id === id) {
+				return {...clothe, favorite: !clothe.favorite};
+			}
+
+			return {...clothe};
+		});
+
+		setClothes(newClothes);
 	}
 
 	return (
 		<div className='list-container'>
-			<ul className='list'>{
-				categories.map((cat, index) => <Categories
-					handleClickList={(index: number) => {
-						handleClickList(index);
-					}}
-					categories={cat}
-					key={cat.category}
-					index={index} />)
-			}</ul>
+			<ul className='list'>
+				{categories.map((cat, index) =>
+					<Categories
+						key={cat.category}
+						categories={cat}
+						index={index}
+						clothes={clothes.filter(clothe =>
+							cat.category === 'TODOS'
+								? true
+								: cat.category === 'FAVORITOS'
+									? clothe.favorite
+									: clothe.category === cat.category,
+						)}
+						handleClickList={() => {
+							handleClickList(index);
+						}}
+						handleClickFavorite={(id: number) => {
+							handleClickFavorite(id);
+						}}
+					/>,
+				)}
+			</ul>
 		</div>
 	);
 }
--- frontend/src/pages/Home/Home.tsx
+++ frontend/src/pages/Home/Home.tsx
@@ -3,11 +3,18 @@ import Looks from '../../components/Looks/Looks';
 import './Home.css';
 
 function Home() {
+	function logOut() {
+		console.log('Log Out');
+	}
+
 	return (
 		<main>
 			<header>
 				<h2>LOOK</h2>
 				<h1>TODAYS LOOK</h1>
+				<button onClick={() => {
+					logOut();
+				}}>Sign out</button>
 			</header>
 			<Looks />
 		</main>
