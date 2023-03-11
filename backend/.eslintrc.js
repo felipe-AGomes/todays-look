96805182be99c960d8c385dcf925f4f39e3afa65
Felipe <felipeagomes@Felipe>
2023-03-11 20:42:08 -0300

    initial commit backend

---
+++ backend/.eslintrc.js
@@ -0,0 +1,26 @@
+module.exports = {
+	env: {
+		es2021: true,
+		node: true,
+	},
+	extends: 'xo',
+	overrides: [
+		{
+			extends: [
+				'xo-typescript',
+			],
+			files: [
+				'*.ts',
+				'*.tsx',
+			],
+		},
+	],
+	parserOptions: {
+		ecmaVersion: 'latest',
+		sourceType: 'module',
+		Project: 'tsconfig.json',
+		tsconfigRootDir: __dirname,
+	},
+	rules: {
+	},
+};
